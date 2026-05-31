2d5162c6b2e3d837457abd2eca8f5c506673f777
Moahmemd_Shak <Mohammedshak055@gmail.com>
2026-05-31 21:55:27 +0300

    .

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('sql208.infinityfree.com', 'if0_42063830', 'a1s1d1f1g1h1@@', 'if0_42063830_lab4_ai_site');
+$conn = mysqli_connect('sql208.infinityfree.com', 'if0_42063830', 'A1s1d1f1g1h11', 'if0_42063830_lab4_ai_site');
 if (!$conn) {
     die('Connection failed: ' . mysqli_connect_error());
 }
